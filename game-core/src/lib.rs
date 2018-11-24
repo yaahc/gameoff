327fee7de1193f012b35df9ffa69503c3a89a379
Jane Lusby <jlusby42@gmail.com>
2018-11-24 15:22:00 -0800

    WIP

--- game-core/src/lib.rs
+++ game-core/src/lib.rs
@@ -8,8 +8,52 @@ extern crate rand;
 mod component;
 mod load;
 mod map;
-pub mod state;
-pub mod system;
+mod state;
+mod system;
+
+use amethyst::{
+    core::TransformBundle,
+    input::InputBundle,
+    prelude::*,
+    renderer::{
+        ColorMask, DepthMode, DisplayConfig, DrawSprite, Pipeline, RenderBundle, Stage, ALPHA,
+    },
+    utils::application_root_dir,
+};
+use state::Menu;
+
+pub fn run() -> amethyst::Result<()> {
+    let root = format!("{}/resources", application_root_dir());
+    let config = DisplayConfig::load(format!("{}/display_config.ron", root));
+    let pipe = Pipeline::build().with_stage(
+        Stage::with_backbuffer()
+            .clear_target([0.1, 0.1, 0.1, 1.0], 1.0)
+            .with_pass(DrawSprite::new().with_transparency(
+                ColorMask::all(),
+                ALPHA,
+                Some(DepthMode::LessEqualWrite), // Tells the pipeline to respect sprite z-depth
+            )),
+    );
+
+    let game_data = GameDataBuilder::default()
+        .with_bundle(TransformBundle::new())?
+        .with_bundle(
+            InputBundle::<String, String>::new()
+                .with_bindings_from_file(format!("{}/input.ron", root))?,
+        )?.with(
+            amethyst::utils::ortho_camera::CameraOrthoSystem::default(),
+            "OrthoCamera",
+            &[],
+        ).with_bundle(
+            RenderBundle::new(pipe, Some(config))
+                .with_sprite_sheet_processor()
+                .with_sprite_visibility_sorting(&[]), // Let's us use the `Transparent` component
+        )?;
+
+    let mut game = Application::build(root, Menu)?.build(game_data)?;
+    game.run();
+    Ok(())
+}
 
 #[cfg(test)]
 mod tests {
---
+++ game-core/src/state/menu.rs
@@ -0,0 +1,77 @@
+use amethyst::{
+    core::Transform,
+    ecs::prelude::*,
+    ecs::Entity,
+    prelude::*,
+    renderer::Camera,
+    utils::ortho_camera::{CameraNormalizeMode, CameraOrtho},
+};
+use component::{Animation, Player};
+use crate::load;
+use crate::state::Game;
+use crate::system::*;
+
+pub struct Menu;
+
+impl SimpleState<'static, 'static> for Menu {
+    fn on_start(&mut self, data: StateData<GameData>) {
+        let world = data.world;
+
+        world.register::<Player>();
+        world.register::<Animation>();
+
+        world.add_resource(load::LoadedTextures::default());
+
+        let player_sprite_sheet_handle = load::sprite_sheet(world, "FRONT.png", "FRONT.ron");
+        let _penguin_sprite_sheet_handle =
+            load::sprite_sheet(world, "penguinFront.png", "penguinFront.ron");
+        let _ = load::sprite_sheet(world, "bubble.png", "bubble.ron");
+
+        crate::map::load_map_sprites(world);
+        let parent = Player::new(world, &player_sprite_sheet_handle);
+        init_camera(world, parent);
+    }
+
+    fn update(
+        &mut self,
+        data: &mut StateData<GameData>,
+    ) -> Trans<GameData<'static, 'static>, StateEvent> {
+        let world = &mut data.world;
+        let mut dispatcher: Dispatcher<'static, 'static> = DispatcherBuilder::new()
+            .with(player::Movement, "player-movement", &[])
+            .with(enemy::Movement, "enemy-movement", &[])
+            .with(camera::Movement, "camera-movement", &[])
+            .with(enemy::Spawner, "enemy-spawner", &[])
+            .with(ally::Movement, "ally-movement", &[])
+            .with(ally::Grouper, "ally-grouper", &[])
+            .with(ally::Spawner, "ally-spawner", &[])
+            .with(player::Attack, "player-attack", &[])
+            .with(animation::Frame, "frame-animation", &[])
+            .with(projectile::Movement, "projectile-movement", &[])
+            .build();
+        dispatcher.setup(&mut world.res);
+        Trans::Push(Box::new(Game { dispatcher }))
+    }
+}
+
+pub fn init_camera(world: &mut World, parent: Entity) {
+    let mut transform = {
+        let transforms = world.read_storage::<Transform>();
+        transforms.get(parent).unwrap().clone()
+    };
+
+    world.register::<CameraOrtho>();
+
+    transform.translation.z = 2.0;
+    transform.translation.x -= 256.0;
+    transform.translation.y -= 256.0;
+    transform.scale.x = 512.0;
+    transform.scale.y = 512.0;
+
+    world
+        .create_entity()
+        .with(CameraOrtho::normalized(CameraNormalizeMode::Contain))
+        .with(Camera::standard_2d())
+        .with(transform)
+        .build();
+}
